f4751d758c8abd6b023259cef272de21cd56cd84
Dominik Gyulai <gyulai.dominik.mark@gmail.com>
2021-11-04 12:35:35 +0100

    Added lombok to the project and refactored the code

--- src/main/java/com/example/CMS/model/AppUser.java
+++ src/main/java/com/example/CMS/model/AppUser.java
@@ -1,44 +1,17 @@
 package com.example.CMS.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AppUser {
 
     private Long userId;
     private String userName;
     private String encrytedPassword;
-
-    public AppUser() {
-
-    }
-
-    public AppUser(Long userId, String userName, String encrytedPassword) {
-        this.userId = userId;
-        this.userName = userName;
-        this.encrytedPassword = encrytedPassword;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getEncrytedPassword() {
-        return encrytedPassword;
-    }
-
-    public void setEncrytedPassword(String encrytedPassword) {
-        this.encrytedPassword = encrytedPassword;
-    }
 
     @Override
     public String toString() {
