116caacd90ce3740b4d4940105165bc19f705639
polgar janos <polgarjano@gmail.com>
2021-11-05 09:22:46 +0100

    hirdetes listazo page

--- src/main/java/com/example/CMS/service/JobAdvertisementService.java
+++ src/main/java/com/example/CMS/service/JobAdvertisementService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -23,5 +24,9 @@ public class JobAdvertisementService {
             return jA.get();
         }
 
+    }
+
+    public List<JobAdvertisement> jobAdvertisements(){
+        return jobAdvertisementRepo.findAll();
     }
 }
