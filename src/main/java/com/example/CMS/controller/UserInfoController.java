52a79befcfe2acc0b3c394666b5faac59806c665
polgar janos <polgarjano@gmail.com>
2021-11-24 08:34:18 +0100

    disassemble the MainControler

---
+++ src/main/java/com/example/CMS/controller/UserInfoController.java
@@ -0,0 +1,29 @@
+package com.example.CMS.controller;
+
+import com.example.CMS.utils.WebUtils;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.security.Principal;
+
+@Controller
+public class UserInfoController {
+    @RequestMapping(value = "/userInfo", method = RequestMethod.GET)
+    public String userInfo(Model model, Principal principal) {
+
+        String userName = principal.getName();
+
+        System.out.println("User Name: " + userName);
+
+        User loginedUser = (User) ((Authentication) principal).getPrincipal();
+
+        String userInfo = WebUtils.toString(loginedUser);
+        model.addAttribute("userInfo", userInfo);
+
+        return "userInfoPage";
+    }
+}
